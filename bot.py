91343706575fa501083e4474c1445bdcd61cacfd
DarthAluminium <43782012+DarthAluminium@users.noreply.github.com>
2020-04-07 00:49:39 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,7 +1,6 @@
 #sleephelperbot
 import json
 import requests
-import datetime
 
 class WABot():
 def __init__(self, json):
@@ -9,5 +8,12 @@ def __init__(self, json):
     self.dict_messages = json['messages']
     self.APIUrl = 'https://eu108.chat-api.com/instance114187/'
     self.token = 'mjl2s79xvdjnyykc'
+
+def send_requests(self, method, data):
+    url = f"{self.APIUrl}{method}?token={self.token}"
+    headers = {'Content-type': 'application/json'}
+    answer = requests.post(url, data=json.dumps(data), headers=headers)
+    return answer.json()
+
 def show_chat_id(self,chatID):
-    return self.send_message(chatID, f"Chat ID : {chatID}")
+    return self.send_message(chatID, f"Chat ID : {79870016999@c.us}")
