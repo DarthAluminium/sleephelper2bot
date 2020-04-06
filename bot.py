aa4885ef0986b076f27b0f475b396cb5ffdcfe76
DarthAluminium <43782012+DarthAluminium@users.noreply.github.com>
2020-04-07 01:16:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,5 +15,5 @@ def send_requests(self, method, data):
     answer = requests.post(url, data=json.dumps(data), headers=headers)
     return answer.json()
 
-def show_chat_id(self,chatID):
-    return self.send_message(chatID, f"Chat ID : {79870016999@c.us}")
+'''def show_chat_id(self,chatID):
+    return self.send_message(chatID, f"Chat ID : {79870016999@c.us}")'''
