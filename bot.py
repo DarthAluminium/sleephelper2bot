7e683deefe1f22c669507b1f4308e20c3d7fbb5d
DarthAluminium <43782012+DarthAluminium@users.noreply.github.com>
2020-04-07 01:30:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ def send_requests(self, method, data):
     url = f"{self.APIUrl}{method}?token={self.token}"
     headers = {'Content-type': 'application/json'}
     answer = requests.post(url, data=json.dumps(data), headers=headers)
-    return.dict_messages
+    return dict_messages
     return answer.json()
 
 '''def show_chat_id(self,chatID):
